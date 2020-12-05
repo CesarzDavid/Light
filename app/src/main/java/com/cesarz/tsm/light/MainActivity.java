cd50921aca71a516fc376ce8ebd60d25682722ce
Cesar Cruz <cesardavidcp@comunidad.unam.mx>
2020-12-04 21:19:17 -0600

    Intentando hacer conexión con el servidor

--- app/src/main/java/com/cesarz/tsm/light/MainActivity.java
+++ app/src/main/java/com/cesarz/tsm/light/MainActivity.java
@@ -2,11 +2,17 @@ package com.cesarz.tsm.light;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,16 +28,54 @@ public class MainActivity extends AppCompatActivity {
 
     public void ButtonStatus(View view)
     {
-        Toast.makeText(MainActivity.this, "Status Done", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(MainActivity.this, "Status Done", Toast.LENGTH_SHORT).show();
+        getJSON("https://tsmpjgv9.000webhostapp.com/luz.php");
     }
     public void ButtonOn(View view)
     {
         ImageFoco.setBackgroundResource(R.drawable.buttonon);
         //Toast.makeText(MainActivity.this, "on", Toast.LENGTH_SHORT).show();
+        getJSON("https://tsmpjgv9.000webhostapp.com/lon.php");
     }
     public void ButtonOff(View view)
     {
         ImageFoco.setBackgroundResource(R.drawable.buttonoff);
         //Toast.makeText(MainActivity.this, "off", Toast.LENGTH_SHORT).show();
+        getJSON("https://tsmpjgv9.000webhostapp.com/loff.php");
+    }
+
+    private void getJSON(final String urlWebService) {
+
+        class GetJSON extends AsyncTask<Void, Void, String> {
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            protected String doInBackground(Void... voids) {
+                try {
+                    URL url = new URL(urlWebService);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    StringBuilder sb = new StringBuilder();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    String json;
+                    while ((json = bufferedReader.readLine()) != null) {
+                        sb.append(json + "\n");
+                    }
+                    return sb.toString().trim();
+                } catch (Exception e) {
+                    return null;
+                }
+
+            }
+        }
+
     }
 }
